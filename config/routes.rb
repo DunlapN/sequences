6ed3923028980e00763cf7664ed097a81fac9c68
DunlapN <DunlapN@users.noreply.github.com>
2017-01-31 19:44:18 -0600

    Done with intial routing and copy pasting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/", { :controller => "sequence", :action => "all_guesses"})
+  get("/all_guesses", { :controller => "sequence", :action => "all_guesses"})
+  get("/show_answer", { :controller => "sequence", :action => "show_answer"})
+
+
+
+
+
 end
